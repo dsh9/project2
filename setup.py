abadcbba2313748e295094bbfac0e02ba6c98396
David Hatch <dshatch2@dshwebart.com>
2013-04-01 11:31:28 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Distutils',
+version='1.0',
+description='Python Distribution Utilities',
+author='dsh',
+author_email='dsh@python.net',
+url='http://www.python.org/sigs/distutils-sig/',
+packages=['distutils', 'distutils.command'],
+)
